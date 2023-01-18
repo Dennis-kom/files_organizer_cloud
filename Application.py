f8288ab24ca6924fad39b81c48147b58d1cd23ff
Dennis <energizer557@gmail.com>
2023-01-18 19:20:14 +0200

    first files structure

---
+++ Application.py
@@ -0,0 +1,6 @@
+import main
+import tkinter
+import Driver
+#TODO: this is will be the dispatch place with GUI - need to design
+
+
---
+++ Driver.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import threading
+
+
+class Driver:
+
+    def __init__(self):
+        pass
+
+    #TODO: define how to manage configurations
+    def load_from_configuration(self):
+        pass
+
